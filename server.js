e96f5cb9f3c8fdb7c348d53ab679f0b73839dc63
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-07 11:24:08 -0400

    moved api data to API folder and json file

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 8000
+const PORT = 7000
+const API = require('./API/games.json')
 
 app.use(cors())
-
-let games = {
-  'world of warcraft': {
-    'name': 'World of Warcraft',
-    'genre': 'mmorpg',
-    'year': '2004'
-  },
-  'call of duty': {
-    'name': 'Call of Duty',
-    'genre': 'fps',
-    'year': '2006'
-  },
-  'monster hunter rise': {
-    'name': 'Monster Hunter Rise',
-    'genre': 'arpg',
-    'year': '2021'
-  },
-  'not added': {
-    'name': 'unknown',
-    'genre': 'unknown',
-    'year': 'unknown'
-  }
-}
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html')
 })
 
+app.get('/api/games', (req, res) => {
+  res.json(API)
+})
+
 app.get('/api/games/:game', (request, response) => {
   const gameInfo = request.params.game.toLowerCase()
   console.log(gameInfo)
-  response.json(games[gameInfo])
+  response.json(API[gameInfo])
 })
 
 app.listen(process.env.PORT || PORT, () => {
