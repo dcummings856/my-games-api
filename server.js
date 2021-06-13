a351a99870c39aa2be9595440cc207431cee2f4c
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-13 17:48:41 -0400

    added index file

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ require("dotenv").config({ path: "./config/.env" })
 
 connectDB()
 
+app.set('view engine', 'ejs')
+
 app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 app.use('/api', gamesRoutes)
 
 app.listen(process.env.PORT || PORT, () => {
